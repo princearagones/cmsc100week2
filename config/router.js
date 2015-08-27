c71db231c1055434cc140e48ba5be515787211d7
princearagones <princearagones@gmail.com>
2015-08-27 15:29:14 +0800

    cmsc100week3 addition

--- config/router.js
+++ config/router.js
@@ -2,11 +2,20 @@ var student = require('./../controllers/student');
 var teacher = require('./../controllers/teacher');
 
 module.exports = function(router){ //if you want to export from another modules and other functions
-	router.route('/students').get(student.find);
-	router.route('/teachers').get(teacher.view);
-	router.route('/teachers').post(teacher.add);
-	router.route('/teachers').put(teacher.update);
-	router.route('/teachers').delete(teacher.delete);
+	router.route('/students')
+		.get(student.find)
+		.post(student.insert);
+	router.route('/students/:id')
+		.get(student.findOne)
+		.put(student.update)
+		.delete(student.remove);
+	
+	router.route('/teachers')
+		.get(teacher.view)
+		.post(teacher.add)
+		.put(teacher.update)
+		.delete(teacher.delete);
+	
   
 	return router;
 };
